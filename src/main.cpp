47ff095471ac17aad62d4358928b43287f95e194
maxsupermanhd <q3.max.2011@ya.ru>
2022-08-20 20:36:15 +0300

    A bit more advanced hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 #include "raylib.h"
+#include "imgui.h"
+#include "rlImGui.h"
 
 int main() {
 	InitWindow(800, 450, "raylib [core] example - basic window");
 	SetTargetFPS(85);
 	SetWindowMonitor(0);
+	rlImGuiSetup(true);
+	bool open = true;
 
 	while(!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+		rlImGuiBegin();
+		ImGui::ShowDemoWindow(&open);
+		rlImGuiEnd();
+
 		EndDrawing();
 	}
 
